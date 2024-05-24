1009a6d64a45bd4980bb4512058a9a357be5cbfb
Yihang Zhu <yihangz@umich.edu>
2024-05-24 17:20:14 +0800

    Order submit

---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("UserOrderController")
+@RequestMapping("/user/order")
+@Api(tags = "order interface")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     * Order submit
+     *
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    @ApiOperation("Submit")
+    public Result<OrderSubmitVO> submit(OrdersSubmitDTO ordersSubmitDTO) {
+        OrderSubmitVO orderSubmitVO = orderService.submitOrder(ordersSubmitDTO);
+        return Result.success(orderSubmitVO);
+    }
+}
